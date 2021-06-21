6f140bb79d3719e742ff297ef6bc75b13cc284e5
spezdmtest <spezd.m.@gmail.com>
2021-06-21 20:48:48 +0300

    Initial commit of the project.

---
+++ src/com/spezdmtest/javacore/chapter18/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+package com.spezdmtest.javacore.chapter18;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi, I\'m your first java program. Stay turned!");
+    }
+}
