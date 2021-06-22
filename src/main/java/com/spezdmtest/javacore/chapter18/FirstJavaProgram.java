e4d97456b26235d35b69fbd0162414a77a2dda56
spezdmtest <spezd.m.@gmail.com>
2021-06-22 10:23:50 +0300

    delete files.  Lines starting

--- src/main/java/com/spezdmtest/javacore/chapter18/FirstJavaProgram.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.spezdmtest.javacore.chapter18;
-
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.println("Hi, I\'m your first java program. Stay turned!");
-    }
-}
--- src/main/java/com/spezdmtest/javacore/chapter18/SecondJavaProgram.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.spezdmtest.javacore.chapter18;
-
-public class SecondJavaProgram {
-    public static void main(String[] args) {
-        System.out.println("This is second program.");
-    }
-}
-
