fb290cf908ac6a5023db151b8ae275d98937b319
spezdmtest <spezd.m.@gmail.com>
2021-07-05 11:35:28 +0300

    Exercise №2

--- src/main/java/com/spezdmtest/javacore/chapter18/Exercise2/AnalizatorDemo.java
+++ src/main/java/com/spezdmtest/javacore/chapter18/Exercise2/AnalizatorDemo.java
@@ -3,6 +3,6 @@ package com.spezdmtest.javacore.chapter18.Exercise2;
 public class AnalizatorDemo {
     public static void main(String[] args) {
         System.out.println(Analizator.analizator("[({})]"));
-        System.out.println(Analizator.analizator("[(){}]"));
+        System.out.println(Analizator.analizator("[(){}"));
     }
 }
