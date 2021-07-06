c5841fbdbd7638b5148d1984c3d75345996b1ad3
spezdmtest <spezd.m.@gmail.com>
2021-07-06 16:38:46 +0300

    Exercise №2

--- src/main/java/com/spezdmtest/javacore/chapter18/Exercise2/AnalyserBracketsDemo.java
+++ src/main/java/com/spezdmtest/javacore/chapter18/Exercise2/AnalyserBracketsDemo.java
@@ -1,6 +1,6 @@
 package com.spezdmtest.javacore.chapter18.Exercise2;
 
-public class AnalizerBracketsDemo {
+public class AnalyserBracketsDemo {
     public static void main(String[] args) {
         System.out.println(AnalyzerBrackets.analyzerBrackets("[({})]"));
         System.out.println(AnalyzerBrackets.analyzerBrackets("[(){}"));
