14ed669409fe829f0a0d89f4559811c515d15f13
spezdmtest <spezd.m.@gmail.com>
2021-07-06 15:39:12 +0300

    Exercise №1

--- src/main/java/com/spezdmtest/javacore/chapter18/Exercise1/Algorithsm.java
+++ src/main/java/com/spezdmtest/javacore/chapter18/Exercise1/Algorithsm.java
@@ -3,9 +3,9 @@ package com.spezdmtest.javacore.chapter18.Exercise1;
 import java.util.LinkedHashSet;
 import java.util.TreeSet;
 
-public class Algorithsm <Integer> {
-    public TreeSet<Integer> mergeCollection(LinkedHashSet<Integer> list1, LinkedHashSet<Integer> list2) {
-        TreeSet<Integer> treeSet = new TreeSet<>();
+public class Algorithsm <T> {
+    public TreeSet<T> mergeCollection(LinkedHashSet<T> list1, LinkedHashSet<T> list2) {
+        TreeSet<T> treeSet = new TreeSet<>();
 //        for (int i = 0; i < arrayList1.size(); i++) {
 //            for(int j = 0; j < arrayList2.size(); j++) {
 //                if(arrayList1.get(i).equals(arrayList2.get(j)) == true) {
@@ -14,8 +14,8 @@ public class Algorithsm <Integer> {
 //                }
 //            }
 //        }
-        for (Integer l1 : list1) {
-            for (Integer l2 : list2) {
+        for (T l1 : list1) {
+            for (T l2 : list2) {
                 if (l1.equals(l2)) {
                     treeSet.add(l1);
                 }
