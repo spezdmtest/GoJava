94578f201b871c04b78d8d2bcadccbfdd1e6ae23
spezdmtest <spezd.m.@gmail.com>
2021-07-08 10:09:30 +0300

    Exercise №1

--- src/main/java/com/spezdmtest/javacore/chapter18/Exercise1/CompareCollections_StreamAPI.java
+++ src/main/java/com/spezdmtest/javacore/chapter18/Exercise1/CompareCollections_StreamAPI.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 public class CompareCollections_StreamAPI<T> {
 
     public static List<Integer> compareCollections(List<Integer> list1, List<Integer> list2) {
-        return   list1.stream()
+        return  list1.stream()
                 .filter(list2::contains)
                 .collect(Collectors.toList());
     }
     public static List<Integer> sortCollection(List<Integer> list) {
-        return list.stream()
+        return  list.stream()
                 .sorted()
                 .collect(Collectors.toList());
     }
