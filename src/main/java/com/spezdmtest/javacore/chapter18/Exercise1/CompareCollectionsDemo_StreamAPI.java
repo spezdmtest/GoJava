628d19da4adea8b6c45d49c45847facc3bc76aaf
spezdmtest <spezd.m.@gmail.com>
2021-07-08 10:07:07 +0300

    Exercise №1

--- src/main/java/com/spezdmtest/javacore/chapter18/Exercise1/CompareCollectionsDemo_StreamAPI.java
+++ src/main/java/com/spezdmtest/javacore/chapter18/Exercise1/CompareCollectionsDemo_StreamAPI.java
@@ -3,9 +3,15 @@ package com.spezdmtest.javacore.chapter18.Exercise1;
 import java.util.List;
 
 public class CompareCollectionsDemo_StreamAPI {
+    public static List<Integer> getList1() {
+        return List.of(6, 4, 6, 8, 1, 100, -100);
+    }
+    public static List<Integer> getList2() {
+        return List.of(3, 8, 76544, -42, 100, 1);
+    }
     public static void main(String[] args) {
-        List<Integer> list1 = CompareCollections_StreamAPI.getList1();
-        List<Integer> list2 = CompareCollections_StreamAPI.getList2();
+        List<Integer> list1 = getList1();
+        List<Integer> list2 = getList2();
 
         System.out.println("Входные данные: ");
         System.out.println(list1);
--- src/main/java/com/spezdmtest/javacore/chapter18/Exercise1/CompareCollections_StreamAPI.java
+++ src/main/java/com/spezdmtest/javacore/chapter18/Exercise1/CompareCollections_StreamAPI.java
@@ -4,12 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CompareCollections_StreamAPI<T> {
-    public static List<Integer> getList1() {
-        return List.of(6, 4, 6, 8, 1, 100, -100);
-    }
-    public static List<Integer> getList2() {
-        return List.of(3, 8, 76544, -42, 100, 1);
-    }
+
     public static List<Integer> compareCollections(List<Integer> list1, List<Integer> list2) {
         return   list1.stream()
                 .filter(list2::contains)
