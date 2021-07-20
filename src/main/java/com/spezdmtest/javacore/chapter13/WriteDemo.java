0827aa7784fc88db6b6472058a5e780c3acc9293
spezdmtest <spezd.m.@gmail.com>
2021-07-20 16:02:50 +0300

    WriteDemo.

---
+++ src/main/java/com/spezdmtest/javacore/chapter13/WriteDemo.java
@@ -0,0 +1,11 @@
+package com.spezdmtest.javacore.chapter13;
+
+public class WriteDemo {
+    public static void main(String[] args) {
+        int b;
+
+        b = 'A';
+        System.out.write(b);
+        System.out.println('\n');
+    }
+}
