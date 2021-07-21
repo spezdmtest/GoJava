d749b2252ac64d0289b1139df189838590e220c7
spezdmtest <spezd.m.@gmail.com>
2021-07-21 10:10:09 +0300

    WriterDemo.

--- src/main/java/com/spezdmtest/javacore/chapter13/WriteDemo.java
+++ src/main/java/com/spezdmtest/javacore/chapter13/WriteDemo.java
@@ -3,7 +3,6 @@ package com.spezdmtest.javacore.chapter13;
 public class WriteDemo {
     public static void main(String[] args) {
         int b;
-
         b = 'A';
         System.out.write(b);
         System.out.println('\n');
