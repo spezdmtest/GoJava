cb33f2bbfd4a5f96f56bfd30342e3fb23ce107d5
spezdmtest <spezd.m.@gmail.com>
2021-07-25 10:26:51 +0300

    Hypot.

--- src/main/java/com/spezdmtest/javacore/chapter13/Hypot.java
+++ src/main/java/com/spezdmtest/javacore/chapter13/Hypot.java
@@ -8,7 +8,6 @@ public class Hypot {
         double hypot;
         side1 = 3.0;
         side2 = 4.0;
-
         hypot = Math.sqrt(Math.pow(side1, 2) +
                           Math.pow(side2, 2));
         System.out.println("При заданной длине сторон " + side1 + " и " + side2 +
