7a5fb555ad876bc16e97a868f88fc57388916c37
spezdmtest <spezd.m.@gmail.com>
2021-07-25 10:26:17 +0300

    Hypot

---
+++ src/main/java/com/spezdmtest/javacore/chapter13/Hypot.java
@@ -0,0 +1,17 @@
+package com.spezdmtest.javacore.chapter13;
+
+import javax.imageio.stream.ImageInputStream;
+
+public class Hypot {
+    public static void main(String[] args) {
+        double side1, side2;
+        double hypot;
+        side1 = 3.0;
+        side2 = 4.0;
+
+        hypot = Math.sqrt(Math.pow(side1, 2) +
+                          Math.pow(side2, 2));
+        System.out.println("При заданной длине сторон " + side1 + " и " + side2 +
+                            "гипотинуза равна " + hypot);
+    }
+}
