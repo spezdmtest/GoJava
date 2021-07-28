785a1f8b5bc3a946ce351dd134590c5ce7ca442e
spezdmtest <spezd.m.@gmail.com>
2021-07-28 11:24:17 +0300

    VarCapture.

---
+++ src/main/java/com/spezdmtest/javacore/chapter15/VarCapture.java
@@ -0,0 +1,14 @@
+package com.spezdmtest.javacore.chapter15;
+interface MyFunc {
+    int func(int n);
+}
+public class VarCapture {
+    public static void main(String[] args) {
+        int num = 10;
+
+        MyFunc myLambda = (n) -> {
+            int v = num + n;
+            return v;
+        };
+    }
+}
