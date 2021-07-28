8173a4e7857e3899e9a00caf7c56dec68f220a5e
spezdmtest <spezd.m.@gmail.com>
2021-07-28 10:05:58 +0300

    LambdasAsArgumentsDemo.

---
+++ src/main/java/com/spezdmtest/javacore/chapter15/LambdasAsArgumentsDemo.java
@@ -0,0 +1,41 @@
+package com.spezdmtest.javacore.chapter15;
+interface StingFunc {
+    String func (String m);
+}
+public class LambdasAsArgumentsDemo {
+    static String stringOp(StringFunc sf, String s) {
+        return sf.func(s);
+    }
+
+    public static void main(String[] args) {
+        String inStr = "Лямбда-выражения повышают эффективность Java";
+        String  outStr;
+
+        System.out.println("Это исходная строка: " + inStr);
+        outStr = stringOp((str) -> str.toUpperCase(),inStr);
+        System.out.println("Это строка в верхнем регистре: " + outStr);
+
+        outStr = stringOp((str) -> {
+            String result = " ";
+            int i;
+
+            for(i = 0; i < str.length(); i++)
+                if(str.charAt(i) != ' ')
+                    result += str.charAt(i);
+                return result;
+        }, inStr);
+    System.out.println("Это строка с удаленным пробелами: " + outStr);
+
+
+        StringFunc reverse = (str) -> {
+            String result  = " ";
+            int i;
+            for(i = str.length()-1; i >= 0; i--)
+                result += str.charAt(i);
+            return result;
+        };
+
+        System.out.println("Это обращенная строка: " +
+                            stringOp(reverse,inStr));
+    }
+}
