d1dd9d5389bdb2a29ea2f695c54b0d4b9389093a
spezdmtest <spezd.m.@gmail.com>
2021-07-28 21:20:09 +0300

    InstanceMethWithObjectRefDemo.

--- src/main/java/com/spezdmtest/javacore/chapter15/InstanceMethWithObjectRefDemo.java
+++ src/main/java/com/spezdmtest/javacore/chapter15/InstanceMethWithObjectRefDemo.java
@@ -20,19 +20,19 @@ class HighTemp {
 }
 
 public class InstanceMethWithObjectRefDemo  {
-  static <T> int counter(T[] vals, MyFunc2<T> f, T v) {
-      int count = 0;
-      for(int i = 0; i < vals.length; i++)
-        if(f.func(vals[i],v)) count++;
-    return count;
-  }
+    static <T> int counter(T[] vals, MyFunc2<T> f, T v) {
+        int count = 0;
+        for(int i = 0; i < vals.length; i++)
+            if(f.func(vals[i],v)) count++;
+        return count;
+    }
 
     public static void main(String[] args) {
         int count;
         HighTemp[]  weekDayHighs = {new HighTemp(89),new HighTemp(82),
-                                    new HighTemp(90),new HighTemp(89),
-                                    new HighTemp(89),new HighTemp(91),
-                                    new HighTemp(84),new HighTemp(83) };
+                new HighTemp(90),new HighTemp(89),
+                new HighTemp(89),new HighTemp(91),
+                new HighTemp(84),new HighTemp(83) };
 
         count = counter(weekDayHighs, HighTemp::sameTemp, new HighTemp(89));
         System.out.println("Дней, когда максимальная температура была 89: " + count);
