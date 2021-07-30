bd2c076e6d0ab26f2f5aaef24a808f9d4670f75a
spezdmtest <spezd.m.@gmail.com>
2021-07-30 10:05:52 +0300

    FileDemo.

---
+++ src/main/java/com/spezdmtest/javacore/chapter20/FileDemo.java
@@ -0,0 +1,23 @@
+package com.spezdmtest.javacore.chapter20;
+import java.io.File;
+
+public class FileDemo {
+    static void p(String s) {
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        File f1 = new File("C:/Users/spezdm/IdeaProjects/GoJava/src/main/java/COPYRIGHT");
+        p("Имя файла: " + f1.getName());
+        p("Путь: " + f1.getName());
+        p("Абсолютный путь: " + f1.getAbsolutePath());
+        p("Родительский каталог: " + f1.getParent());
+        p(f1.exists() ? "существует" : "не существует");
+        p(f1.canWrite() ? "доступен для записи" : "не доступен для записи");
+        p(f1.canRead() ? "доступен для чтения" : "не доступен для чтения");
+        p(f1.isDirectory() ? "является каталоком" : "не является каталогом");
+        p(f1.isFile() ? "является обычным файлом" : "не является обычным файлом");
+        p(f1.isAbsolute() ? "является абсолютным" : "не является абсолютным");
+        p("Последние изменение в файле" + f1.lastModified());
+        p("Размер: " + f1.length() + "байт");
+    }
+}
