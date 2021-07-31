8240c1e0a5be4cc8e890b40b5815ab06c6c8d937
spezdmtest <spezd.m.@gmail.com>
2021-07-31 16:55:43 +0300

    FileDemo.

---
+++ src/main/java/com/spezdmtest/javacore/chapter20/DirList.java
@@ -0,0 +1,24 @@
+package com.spezdmtest.javacore.chapter20;
+import java.io.File;
+
+public class DirList {
+    public static void main(String[] args) {
+        String dirname = "C:/Users/spezdm/IdeaProjects/GoJava/src/main/java/";
+        File f1 = new File(dirname);
+        if(f1.isDirectory()) {
+            System.out.println("Каталог " + dirname);
+            String s[] = f1.list();
+
+            for(int i=0; i < s.length; i++) {
+                File f = new File(dirname + "/" + s[i]);
+                if (f.isDirectory()) {
+                    System.out.println(s[i] + " является каталогом");
+                } else {
+                    System.out.println(s[i] + " является файлом");
+                }
+            }
+        } else {
+            System.out.println(dirname + " не является каталогом");
+        }
+    }
+}
