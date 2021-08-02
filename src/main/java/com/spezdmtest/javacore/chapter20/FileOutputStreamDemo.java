27cf6b6e9766fe7f07350c004e6e428988dd72d0
spezdmtest <spezd.m.@gmail.com>
2021-08-02 22:27:12 +0300

    FileInputStreamDemo.

--- src/main/java/com/spezdmtest/javacore/chapter20/FileOutputStreamDemo.java
+++ src/main/java/com/spezdmtest/javacore/chapter20/FileOutputStreamDemo.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 public class FileOutputStreamDemo {
     public static void main(String[] args) {
         String source  = "Now is the time for all good men\n" +
-                         " to come to the aid of their country\n" +
-                         " and pay their due taxes.";
+                " to come to the aid of their country\n" +
+                " and pay their due taxes.";
         byte buf[] = source.getBytes();
         FileOutputStream f0 = null;
         FileOutputStream f1 = null;
@@ -22,8 +22,24 @@ public class FileOutputStreamDemo {
             f1.write(buf);
 
             f2.write(buf,buf.length-buf.length/4,buf.length/4);
-            }catch (IOException e) {
+        }catch (IOException e) {
             System.out.println("Произошла ошибка ввода-вывода");
+        } finally {
+            try {
+                if (f0 != null) f0.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка закрытия файла file1.txt");
+            }
+            try {
+                if (f1 != null) f1.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка закрытия файла file2.txt");
+            }
+            try {
+                if (f2 != null) f2.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка закрфтия файла file3.txt");
+            }
         }
     }
 }
