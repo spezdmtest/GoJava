ecac40d1b1e110471bf9be21bc2cbd496dfc1825
spezdmtest <spezd.m.@gmail.com>
2021-08-05 08:33:24 +0300

    ByteArrayInputSteamReset.

--- src/main/java/com/spezdmtest/javacore/chapter20/ByteArrayInputStreamReset.java
+++ src/main/java/com/spezdmtest/javacore/chapter20/ByteArrayInputStreamReset.java
@@ -11,10 +11,10 @@ public class ByteArrayInputStreamReset {
         for(int i = 0; i < 2; i++) {
             int c;
             while ((c = in.read()) != -1) {
-                if(i == 0) {
+                if (i == 0) {
                     System.out.print((char) c);
                 } else {
-                    System.out.print(Character.toUpperCase((char)c));
+                    System.out.print(Character.toUpperCase((char) c));
                 }
             }
             System.out.println();
