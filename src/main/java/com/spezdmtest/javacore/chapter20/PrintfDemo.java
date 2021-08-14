b87ba4fdb3c70a954977d78b616b7a45d455d22b
spezdmtest <spezd.m.@gmail.com>
2021-08-14 16:30:46 +0300

    PrintfDemo.

---
+++ src/main/java/com/spezdmtest/javacore/chapter20/PrintfDemo.java
@@ -0,0 +1,32 @@
+package com.spezdmtest.javacore.chapter20;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class PrintfDemo {
+    public static void main(String[] args)
+    {
+        System.out.println("Ниже приведены некоторые " +
+                            "числовые значения в разных форматах. \n");
+        System.out.printf("Разные числоые форматы: ");
+        System.out.printf("%d %(d %+d %05d\n",3,-3,3,3);
+
+        System.out.println();
+        System.out.printf("Формат чисел с плавающей точкой " +
+                          " по умолчанию: %f\n", 1234567.123);
+        System.out.printf("Формат чисел с плавающей точкой " +
+                          " по умолчанию: %,f\n", 1234567.123);
+        System.out.printf("Формат отрицательных чисел с " +
+                            "плавающей точкой по умолчанию: %,f\n", -1234567.123);
+        System.out.printf("Другой Формат отрицательных чисел с " +
+                            "плавающей точкой: %,(f\n", -1234567.123);
+
+        System.out.println();
+
+        System.out.printf("Выравнивание положительных и " +
+                          "отрицательных числовых значений: \n");
+        System.out.printf("% ,.2f\n",1234567.123);
+        System.out.printf("% ,.2f\n",-
+                1234567.123);
+
+    }
+}
