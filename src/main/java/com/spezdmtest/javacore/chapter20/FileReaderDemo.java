370aa9772f1e50c04a506755e8f13704891d596a
spezdmtest <spezd.m.@gmail.com>
2021-08-16 08:27:31 +0300

    FileReaderDemo.

---
+++ src/main/java/com/spezdmtest/javacore/chapter20/FileReaderDemo.java
@@ -0,0 +1,15 @@
+package com.spezdmtest.javacore.chapter20;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo {
+    public static void main(String[] args) {
+        try (FileReader fr = new FileReader("FileReaderDemo.java")) {
+            int c;
+            while((c = fr.read()) != -1 ) System.out.print((char) c);
+        }catch (IOException e) {
+            System.out.println("Ошибки ввода-вывода: " + e);
+        }
+    }
+}
