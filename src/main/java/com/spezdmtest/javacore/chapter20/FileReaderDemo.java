6efff2fa50bba7c21e346c0317cd04e2846e33d7
spezdmtest <spezd.m.@gmail.com>
2021-08-16 09:30:57 +0300

    FileReaderDemo.

--- src/main/java/com/spezdmtest/javacore/chapter20/FileReaderDemo.java
+++ src/main/java/com/spezdmtest/javacore/chapter20/FileReaderDemo.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 
 public class FileReaderDemo {
     public static void main(String[] args) {
-        try (FileReader fr = new FileReader("FileReaderDemo.java")) {
+        try (FileReader fr = new FileReader("C:/Users/spezdm/IdeaProjects/GoJava/" +
+                "src/main/java/com/spezdmtest/javacore/chapter20/FileReaderDemo.java")) {
             int c;
             while((c = fr.read()) != -1 ) System.out.print((char) c);
         }catch (IOException e) {
