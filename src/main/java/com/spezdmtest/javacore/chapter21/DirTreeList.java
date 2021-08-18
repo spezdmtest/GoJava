a86cb9b0554ceb63947d952a315950cd4d32f55e
spezdmtest <spezd.m.@gmail.com>
2021-08-18 16:40:20 +0300

    DirTreeList.

---
+++ src/main/java/com/spezdmtest/javacore/chapter21/DirTreeList.java
@@ -0,0 +1,25 @@
+package com.spezdmtest.javacore.chapter21;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class MyFileVisitor extends SimpleFileVisitor<Path> {
+    public FileVisitResult visitFile(Path path, BasicFileAttributes attribs) throws IOException {
+        System.out.println(path);
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class DirTreeList {
+    public static void main(String[] args) {
+        String dirname = "example";
+        System.out.println("Дерево каталогов, начиная с каталога " +
+                dirname + ":\n");
+        try {
+            Files.walkFileTree(Paths.get(dirname),new MyFileVisitor());
+        }catch (IOException exc) {
+            System.out.println("Ошибка ввода-вывода");
+        }
+    }
+}
