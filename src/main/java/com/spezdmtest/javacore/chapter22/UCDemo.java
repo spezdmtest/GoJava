f1c162891f469cef1ba935f884783e4fa1ac5ff3
spezdmtest <spezd.m.@gmail.com>
2021-08-19 21:59:17 +0300

    UCDemo.

--- src/main/java/com/spezdmtest/javacore/chapter22/UCDemo.java
+++ src/main/java/com/spezdmtest/javacore/chapter22/UCDemo.java
@@ -10,45 +10,45 @@ public class UCDemo {
     public static void main(String[] args) throws IOException {
         int c;
         URL hp = new URL("http://www.internic.net");
-        URLConnection hpCon= hp.openConnection();
+        URLConnection hpCon = hp.openConnection();
 
         long d = hpCon.getDate();
-        if(d==0)
+        if (d == 0)
             System.out.println("Сведения о дате отсутствуют.");
         else
             System.out.println("Дата: " + new Date(d));
         System.out.println("Тип содержимого: " + hpCon.getContentType());
 
         d = hpCon.getExpiration();
-        if(d==0)
+        if (d == 0)
             System.out.println(
                     "Сведения о сроке действия отсутствуют");
         else
             System.out.println(
-                    "Срок действия истекает: " + new Date(d) );
+                    "Срок действия истекает: " + new Date(d));
 
         d = hpCon.getLastModified();
-        if(d==0)
+        if (d == 0)
             System.out.println(
                     "Сведения о дате последней модификации");
         else
             System.out.println(
-                    "Дата последней модификации: " + new Date(d) );
+                    "Дата последней модификации: " + new Date(d));
 
         long len = hpCon.getContentLength();
-        if(len== -1)
+        if (len == -1)
             System.out.println("Длина содержимого недоступна.");
         else
             System.out.println("Длина содержимого: " + len);
 
-        if(len != 0) {
+        if (len != 0) {
             System.out.println("===Содержимое===");
             InputStream input = hpCon.getInputStream();
             while (((c = input.read()) != -1)) {
                 System.out.print((char) c);
             }
             input.close();
-        }else {
+        } else {
             System.out.println("Содержимое недоступно.");
         }
     }
