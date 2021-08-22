197938a1319e93ab84ed618b47ed6eb64e1c2997
spezdmtest <spezd.m.@gmail.com>
2021-08-22 22:25:58 +0300

    StreamDemo10.

---
+++ src/main/java/com/spezdmtest/javacore/chapter29/StreamDemo10.java
@@ -0,0 +1,30 @@
+package com.spezdmtest.javacore.chapter29;
+
+import java.util.ArrayList;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+
+public class StreamDemo10 {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Альфа");
+        myList.add("Бета");
+        myList.add("Гамма");
+        myList.add("Дельта");
+        myList.add("Кси");
+        myList.add("Омега");
+
+        Stream<String> myStream = myList.stream();
+        Spliterator<String> splitItr = myStream.spliterator();
+        Spliterator<String> splitItr2 = splitItr.trySplit();
+
+        if(splitItr2 != null) {
+            System.out.println("Результат, выводимый итератором splititr2: ");
+            splitItr2.forEachRemaining((n) -> System.out.println(n));
+        }
+
+        System.out.println("\nРезультат, выводимый интератором splitItr: ");
+        splitItr.forEachRemaining((n) -> System.out.println(n));
+    }
+}
+
