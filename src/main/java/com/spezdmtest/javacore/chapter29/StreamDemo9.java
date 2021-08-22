78f974668094257e4bfe142e05253c88904f8e0f
spezdmtest <spezd.m.@gmail.com>
2021-08-22 21:20:29 +0300

    StreamDemo9.

---
+++ src/main/java/com/spezdmtest/javacore/chapter29/StreamDemo9.java
@@ -0,0 +1,23 @@
+package com.spezdmtest.javacore.chapter29;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+
+public class StreamDemo9 {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Альфа");
+        myList.add("Бета");
+        myList.add("Гамма");
+        myList.add("Дельта");
+        myList.add("Кси");
+        myList.add("Омега");
+
+        Stream<String> myStream =myList.stream();
+        Spliterator<String> splitItr = myStream.spliterator();
+        while (splitItr.tryAdvance((n) -> System.out.println(n)));
+        }
+    }
+
