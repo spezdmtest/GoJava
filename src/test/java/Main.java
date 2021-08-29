51b17d708d79ee94f49b3cc4c55a3acdae9bd2d4
spezdmtest <spezd.m.@gmail.com>
2021-08-29 14:48:37 +0300

    Exercise Module_1.3

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,32 +4,26 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) throws IOException {
-        String json = "[{\"id\":1,\"name\":\"one\"},{\"id\":2,\"name\":\"two\"}]";
-        ArrayList<Skill> skills = SkillRepository.givenJsonOfSkill(json);
-        SkillRepository skillRepository = new SkillRepository();
+                String json = "[{\"id\":1,\"name\":\"one\"},{\"id\":2,\"name\":\"two\"}]";
+                ArrayList<Skill> skills = SkillRepository.givenJsonOfSkill(json);
+                SkillRepository skillRepository = new SkillRepository();
 
-        System.out.println("Исходные данные из Json файла: ");
-        skillRepository.Print(skills);
-        //запись исходных данных в файл
-        skillRepository.save(skills);
-        System.out.println();
-        System.out.println("Вывод данных по id ");
-        System.out.println("введите номер id: ");
-        Scanner in = new Scanner(System.in);
-        long num = in.nextLong();
-        System.out.println("Результат: ");
-        System.out.println(skillRepository.getById(num));
-        System.out.println("Вывод всех данных: ");
-        System.out.println(skillRepository.getAll());
-        System.out.println("Обновление данных ");
-        System.out.println(skillRepository.update(skills));
-        System.out.println("Удаления данных ");
-        System.out.println("Введите номер id: ");
-        Scanner in2 = new Scanner(System.in);
-        long num2 = in2.nextLong();
-        System.out.println("Результат: ");
-        skillRepository.deleteById(num2);
-
-
-    }
+                System.out.println("Исходные данные из Json файла: ");
+                skillRepository.Print(skills);
+                //запись исходных данных в файл
+                skillRepository.save(skills);
+                System.out.println();
+                System.out.println("Вывод данных по id ");
+                System.out.println("введите номер id: ");
+                Scanner in = new Scanner(System.in);
+                long num = in.nextLong();
+                System.out.println("Результат: ");
+                System.out.println(skillRepository.getById(num));
+                System.out.println("Вывод всех данных: ");
+                System.out.println(skillRepository.getAll());
+                System.out.println("Обновление данных ");
+                System.out.println(skillRepository.update(skills));
+                System.out.println("Удаления данных ");
+                System.out.println("Введите номер id: ");
+        }
 }
