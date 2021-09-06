31036674a7afb50c0d9aa45377ce320a41030920
spezdmtest <spezd.m.@gmail.com>
2021-09-06 23:38:12 +0300

    Exercise Module_1.3

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
      SkillRepository.save(new Skill((long) 1,"one"));
      SkillRepository.save(new Skill((long) 2,"two"));
      SkillRepository.save(new Skill((long) 3,"three"));
-   System.out.println(SkillRepository.getAll());
+     System.out.println(SkillRepository.getAll());
 //   System.out.println(SkillRepository.getById((long) 3));
     }
 }
