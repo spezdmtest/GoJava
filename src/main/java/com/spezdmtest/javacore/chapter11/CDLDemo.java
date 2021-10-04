c95540ebf14ea33aafecc038bbcdd99e70623b3e
spezdmtest <spezd.m.@gmail.com>
2021-10-04 23:36:10 +0300

    CDLDemo.

--- src/main/java/com/spezdmtest/javacore/chapter11/CDLDemo.java
+++ src/main/java/com/spezdmtest/javacore/chapter11/CDLDemo.java
@@ -25,7 +25,7 @@ class MyThread implements Runnable {
     }
     @Override
     public void run() {
-        for(int i = 0; i < 5; i++) {
+        for(int i = 0; i < 6; i++) {
             System.out.println(i);
             latch.countDown();
         }
