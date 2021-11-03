7c6248628bf108ce2f35f315dd3cb3ed6bc8b9e4
spezdmtest <spezd.m.@gmail.com>
2021-11-03 14:49:06 +0200

    Iterator.

---
+++ src/main/java/com/spezdmtest/javacore/module1_5_3/Iterator/Collection.java
@@ -0,0 +1,5 @@
+package com.spezdmtest.javacore.module1_5_3.Iterator;
+
+public interface Collection {
+    Iterator getIterator();
+}
---
+++ src/main/java/com/spezdmtest/javacore/module1_5_3/Iterator/Iterator.java
@@ -0,0 +1,6 @@
+package com.spezdmtest.javacore.module1_5_3.Iterator;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
