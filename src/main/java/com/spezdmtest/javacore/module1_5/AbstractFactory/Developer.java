e1c7f9359b48c1d9ad2f3d6e5c8320092e05108a
spezdmtest <spezd.m.@gmail.com>
2021-11-05 19:36:40 +0200

    AbstractFactory.

--- src/main/java/com/spezdmtest/javacore/module1_5/AbstractFactory/Developer.java
+++
@@ -1,5 +0,0 @@
-package com.spezdmtest.javacore.module1_5.AbstractFactory;
-
-public interface Developer {
-    void writeCode();
-}
--- src/main/java/com/spezdmtest/javacore/module1_5/AbstractFactory/Tester.java
+++
@@ -1,5 +0,0 @@
-package com.spezdmtest.javacore.module1_5.AbstractFactory;
-
-public interface Tester {
-    void testCode();
-}
